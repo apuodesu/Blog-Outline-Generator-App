c49890ea90eead71848859b19073faadde77737e
Keiji Isogimi <134198796+apuodesu@users.noreply.github.com>
2023-06-09 14:59:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate
 
 st.set_page_config(page_title="🦜🔗 Blog Outline Generator App")
@@ -7,7 +7,7 @@ st.title('🦜🔗 Blog Outline Generator App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 def generate_response(topic):
-    llm = OpenAI(model_name='gpt-4', openai_api_key=openai_api_key)
+    llm = ChatOpenAI(model_name='gpt-4', openai_api_key=openai_api_key)
     # Prompt
     template = '経験豊富なデータサイエンティストであり、技術ライターとして、{topic}についてのブログの概要を作成してください。'
     prompt = PromptTemplate(input_variables=['topic'], template=template)
@@ -23,3 +23,4 @@ with st.form('myform'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
         generate_response(topic_text)
+
