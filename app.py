85f46668d618d842d37935af8ebb52c9a13cff17
Keiji Isogimi <134198796+apuodesu@users.noreply.github.com>
2023-06-09 15:03:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 from langchain import PromptTemplate
 
 st.set_page_config(page_title="🦜🔗 Blog Outline Generator App")
@@ -7,20 +7,19 @@ st.title('🦜🔗 Blog Outline Generator App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 def generate_response(topic):
-    llm = ChatOpenAI(model_name='gpt-4', openai_api_key=openai_api_key)
-    # Prompt
-    template = '経験豊富なデータサイエンティストであり、技術ライターとして、{topic}についてのブログの概要を作成してください。'
-    prompt = PromptTemplate(input_variables=['topic'], template=template)
-    prompt_query = prompt.format(topic=topic)
-    # Run LLM model and print out response
-    response = llm(prompt_query)
-    return st.info(response)
+  llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
+  # Prompt
+  template = '経験豊富なデータサイエンティストであり、技術ライターとして、{topic}についてのブログの概要を作成してください。'
+  prompt = PromptTemplate(input_variables=['topic'], template=template)
+  prompt_query = prompt.format(topic=topic)
+  # Run LLM model and print out response
+  response = llm(prompt_query)
+  return st.info(response)
 
 with st.form('myform'):
-    topic_text = st.text_input('Enter keyword:', '')
-    submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(topic_text)
-
+  topic_text = st.text_input('Enter keyword:', '')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(topic_text)
